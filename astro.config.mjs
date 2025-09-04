c9292824d682170aae53ee3487da629e1c830745
AlanDPA <90456600+AlanDPA@users.noreply.github.com>
2025-09-04 14:08:29 -0500

    1.5

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
     site: 'https://alandpa.github.io',
+    base: 'pan-aroma-landing-page',
     vite: {
         plugins: [tailwindcss()]
     }
